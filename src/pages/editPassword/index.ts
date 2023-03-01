9366067a9d152c883024eafc4330f0159b2afa3c
StepanenkoOlgaV <124494892+StepanenkoOlgaV@users.noreply.github.com>
2023-03-01 15:40:45 +0300

    Update index.ts

--- src/pages/editPassword/index.ts
+++ src/pages/editPassword/index.ts
@@ -1,29 +1,47 @@
 import { Button } from '../../components/button';
 import { FormField } from '../../components/formField';
 import Block from '../../utils/Block';
+import { printValues } from '../../utils/printFormData';
+import { submitValidation } from '../../utils/validation';
 import template from './editPassword.hbs';
 
 export class EditPassword extends Block {
   constructor() {
-    super('box');
+    super('form');
+    this.setProps({  
+    events: {
+      submit: (event: Event) => {event.preventDefault(); 
+       submitValidation(this.children);         
+       printValues(this.children);}
+    },     
+  })
   }
 
   init() {
     this.element?.classList.add('container');
     this.children.editRowOldPassword = new FormField({
-      label: 'Старый пароль', placeholder: '******', type: 'password', name: 'oldPassword',
+      label: 'Старый пароль', 
+      placeholder: '******', 
+      type: 'password', 
+      name: 'oldPassword',
+      validationType: "password",
     });
     this.children.editRowNewPassword = new FormField({
-      label: 'Новй пароль', placeholder: '******', type: 'password', name: 'newPassword',
+      label: 'Новй пароль', 
+      placeholder: '******', 
+      type: 'password', 
+      name: 'newPassword',
+      validationType: "password"
     });
     this.children.editRowPasswordCheck = new FormField({
-      label: 'Повторите новый пароль', placeholder: '******', type: 'password', name: 'password_check',
+      label: 'Повторите новый пароль', 
+      placeholder: '******', 
+      type: 'password', 
+      name: 'password_check',
+      validationType: "password"
     });
     this.children.submitButton = new Button({
-      title: 'Сохранить',
-      events: {
-        click: () => console.log('link'),
-      },
+      title: 'Сохранить',   
     });
   }
 
