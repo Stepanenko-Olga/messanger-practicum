98c0901fb9fb1a2948bed9ae0c54d1c9ca1b64e6
StepanenkoOlgaV <124494892+StepanenkoOlgaV@users.noreply.github.com>
2023-03-01 15:27:23 +0300

    Update index.ts

--- src/pages/chats/messagesBlock/index.ts
+++ src/pages/chats/messagesBlock/index.ts
@@ -1,12 +1,19 @@
 import Block from '../../../utils/Block';
 import template from './messagesBlock.hbs';
+import { MessagesBody } from './messagesBody';
+import { messageAnother, messageMy } from './consts';
+
 import { MessagesFooter } from './messagesFooter';
 import { MessagesHeader } from './messagesHeader';
+
+
 
 export class MessagesBlock extends Block {
   constructor() {
     super('box');
   }
+     
+
 
   init() {
     this.element?.classList.add('messages');
@@ -14,8 +21,16 @@ export class MessagesBlock extends Block {
       name: 'Вадим',
       avatar: 'ava',
     });
+    this.children.messagesBody = new MessagesBody({
+      date: '01 марта',
+      messagesMyText: messageMy.text,
+      messagesAnotherText: messageAnother.text,
+    });
     this.children.messagesFooter = new MessagesFooter({});
   }
+
+
+
 
   render() {
     return this.compile(template, this.props);
