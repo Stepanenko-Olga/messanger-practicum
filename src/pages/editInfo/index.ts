da0c3c40c9916a4dfd1be708eb65261489470c59
StepanenkoOlgaV <124494892+StepanenkoOlgaV@users.noreply.github.com>
2023-03-01 15:39:36 +0300

    Update index.ts

--- src/pages/editInfo/index.ts
+++ src/pages/editInfo/index.ts
@@ -1,11 +1,20 @@
 import { Button } from '../../components/button';
 import { FormField } from '../../components/formField';
 import Block from '../../utils/Block';
+import { printValues} from '../../utils/printFormData';
+import { submitValidation } from '../../utils/validation';
 import template from './editInfo.hbs';
 
 export class EditInfo extends Block {
   constructor() {
     super('form');
+    this.setProps({       
+      events: {
+         submit: (event: Event) => {event.preventDefault(); 
+          submitValidation(this.children);         
+          printValues(this.children);}
+       },     
+     })
   }
 
   init() {
@@ -15,27 +24,45 @@ export class EditInfo extends Block {
       placeholder: 'pochta@yandex.ru',
       type: 'text',
       name: 'email',
+      validationType: "email"
     });
     this.children.editRowLogin = new FormField({
-      label: 'Логин', placeholder: 'ivanivanov', type: 'text', name: 'login',
+      label: 'Логин', 
+      placeholder: 'ivanivanov', 
+      type: 'text', 
+      name: 'login',
+      validationType: "login"
     });
     this.children.editRowFirstName = new FormField({
-      label: 'Имя', placeholder: 'Иван', type: 'text', name: 'first_name',
+      label: 'Имя', 
+      placeholder: 'Иван', 
+      type: 'text', 
+      name: 'first_name',
+      validationType: "name"
     });
     this.children.editRowSecondName = new FormField({
-      label: 'Фамилия', placeholder: 'Иванов', type: 'text', name: 'second_name',
+      label: 'Фамилия', 
+      placeholder: 'Иванов', 
+      type: 'text', 
+      name: 'second_name',
+      validationType: "name"
     });
     this.children.editRowDisplayName = new FormField({
-      label: 'Имя в чате', placeholder: 'Иван', type: 'text', name: 'display_name',
+      label: 'Имя в чате', 
+      placeholder: 'Иван', 
+      type: 'text', 
+      name: 'display_name',
+      validationType: "name"
     });
     this.children.editRowPhone = new FormField({
-      label: 'Телефон', placeholder: '+7 910 123 45 67', type: 'text', name: 'phone',
+      label: 'Телефон', 
+      placeholder: '+7 910 123 45 67', 
+      type: 'text', 
+      name: 'phone',
+      validationType: "phone"
     });
     this.children.submitButton = new Button({
-      title: 'Сохранить',
-      events: {
-        click: () => console.log('link'),
-      },
+      title: 'Сохранить',   
     });
   }
 
