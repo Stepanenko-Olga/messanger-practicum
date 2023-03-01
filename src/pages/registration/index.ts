e175dd30c921e7ac1ae77d24ac0d37c3c5af03bb
StepanenkoOlgaV <124494892+StepanenkoOlgaV@users.noreply.github.com>
2023-03-01 15:42:21 +0300

    Update index.ts

--- src/pages/registration/index.ts
+++ src/pages/registration/index.ts
@@ -2,10 +2,19 @@ import template from './registration.hbs';
 import Block from '../../utils/Block';
 import { Button } from '../../components/button';
 import { FormField } from '../../components/formField';
+import { printValues} from '../../utils/printFormData';
+import { submitValidation } from '../../utils/validation';
 
 export class Registration extends Block {
   constructor() {
-    super('box');
+    super('form');
+    this.setProps({       
+      events: {
+         submit: (event: Event) => {event.preventDefault(); 
+          submitValidation(this.children);         
+          printValues(this.children);}
+       },     
+     })
   }
 
   init() {
@@ -14,49 +23,53 @@ export class Registration extends Block {
       label: 'Почта',
       name: 'email',
       value: 'pochta@yandex.ru',
-      type: 'text',      
+      type: 'text',
+      validationType: "email"      
     });
     this.children.formInputLogin = new FormField({
       label: 'Логин',
       name: 'login',
       value: 'ivanivanov',
-      type: 'text',      
+      type: 'text',
+      validationType: "login"      
     });
     this.children.formInputFirstName = new FormField({
       label: 'Имя',
       name: 'first_name',
       value: 'Иван',
-      type: 'text',      
+      type: 'text',
+      validationType: "name"      
     });
     this.children.formInputSecondName = new FormField({
       label: 'Фамилия',
       name: 'second_name',
       value: 'Иванов',
-      type: 'text',      
+      type: 'text',
+      validationType: "name"       
     });
     this.children.formInputPhone = new FormField({
       label: 'Телефон',
       name: 'phone',
       value: '+7 (910) 123 45 67',
-      type: 'text',      
+      type: 'text',
+      validationType: "phone"       
     });
     this.children.formInputPassword = new FormField({
       label: 'Пароль',
       name: 'password',
       value: '******',
-      type: 'password',      
+      type: 'password',
+      validationType: "password"       
     });
     this.children.formInputPasswordCheck = new FormField({
       label: 'Пароль (еще раз)',
       name: 'password_check',
       value: '******',
-      type: 'password',      
+      type: 'password',
+      validationType: "password"       
     });
     this.children.button = new Button({
-      title: 'Зарегистироваться',
-      events: {
-        click: () => console.log('link'),
-      },
+      title: 'Зарегистироваться',    
     });
   }
 
