c9d799e08412f7b09e020a48b5de6c669ed35513
StepanenkoOlgaV <stepanenko.olga@russianpost.ru>
2023-04-05 13:14:53 +0300

    precommit

--- src/components/formField/index.ts
+++ src/components/formField/index.ts
@@ -46,7 +46,8 @@ export class FormField extends Block {
     switch (validationType) {
       case "login": this.validate(validationMasks.LOGIN, "Введите от 3 до 20 символов на латинице или цифр"); break;
       case "password": this.validate(validationMasks.PASSWORD, "Введите от 8 до 40 символов, обязательно хотя бы одну заглавную букву и цифру"); break;
-      case "name": this.validate(validationMasks.NAME, "Первая буква должна быть заглавной, без пробелов, цифр, спецсимволов (допустим только дефис)"); break;
+      case "name":
+        this.validate(validationMasks.NAME, "Первая буква должна быть заглавной, без пробелов, цифр, спецсимволов (допустим только дефис)"); break;
       case "email": this.validate(validationMasks.EMAIL, "Укажите email латиницей, можно включать цифры и спецсимволы, обязательно должна быть @"); break;
       case "phone": this.validate(validationMasks.PHONE, "Введите от 10 до 15 цифр"); break;
       case "message": this.validate(validationMasks.REQUIRED, "Поле обязательно к заполнению"); break;
