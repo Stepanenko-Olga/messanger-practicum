52fb8719dc70623d023d0fb66d757469a2e6fcd9
StepanenkoOlgaV <stepanenko.olga@russianpost.ru>
2023-04-05 12:52:14 +0300

    Styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 
 module.exports = {
     entry: "./index.ts",
@@ -14,84 +14,74 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.pcss$/,
-                use: [
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    [
-                                        "postcss-import",
-                                        "precss",
-                                        "postcss-simple-vars"
-                                    ],
-                                ],
-                            },
-                        },
+                test: /\.pcss$/i,
+                use: ['style-loader', {
+                    loader: "css-loader",
+                    options: {
+                        modules: true,
+                        importLoaders: 1,
                     },
-              MiniCssExtractPlugin.loader,
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true,
-                            importLoaders: 1,
-                        },
-                    }, 
-                    
-                ],
-            },
-            {
-                test: /\.hbs$/,
-                use: [
-                    {
-                        loader: 'handlebars-template-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.(ts|js)$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'ts-loader',
-                        options: {
-                            configFile: path.resolve(__dirname, 'tsconfig.json'),
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
-        ]
-    },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html'
-        }),
-        new MiniCssExtractPlugin(),
-    ],
+                }, 'postcss-loader'],
+                include: /\.module\.pcss$/i,
+            },
+            {
+                test: /\.pcss$/i,
+                use: ['style-loader', "css-loader", 'postcss-loader'], 
+                exclude: /\.module\.pcss$/i,
+            },
+
+     
+    {
+            
+    test: /\.hbs$/,
+        use: [
+            {
+                loader: 'handlebars-template-loader'
+            }
+        ]
+},
+{
+    test: /\.(ts|js)$/,
+        exclude: /node_modules/,
+            use: [
+                {
+                    loader: 'ts-loader',
+                    options: {
+                        configFile: path.resolve(__dirname, 'tsconfig.json'),
+                    },
+                },
+            ],
+            },
+{
+    test: /\.(png|jpe?g|gif)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    } ]} ,
+
+plugins: [
+    new HtmlWebpackPlugin({
+        template: './index.html'
+    }),
+    new MiniCssExtractPlugin(),
+],
 
     resolve: {
-        extensions: ['.ts', '.js', '.json'],
+    extensions: ['.ts', '.js', '.json'],
         alias: {
-            handlebars: 'handlebars/dist/handlebars.min.js'
-        }
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
+        handlebars: 'handlebars/dist/handlebars.min.js'
+    }
+},
+devServer: {
+    static: {
+        directory: path.join(__dirname, 'dist'),
         },
-        port: 4000,
+    port: 4000,
         historyApiFallback: true,
-        open: true,
-        compress: true,
+            open: true,
+                compress: true,
     },
 };
